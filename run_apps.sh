946b3271e3c7b840efe46ed3ccf1831f2b151ea9
Larry Liu <xiaozi7912@gmail.com>
2024-10-11 16:32:24 +0800

    Modified run_apps.sh

--- run_apps.sh
+++ run_apps.sh
@@ -2,7 +2,7 @@
 
 killall python3
 
-cd /home/igwadmin/Remote_Car
+cd /home/igwadmin/remote-car-server
 
 python3 ./python/car_http.py >/dev/null 2>&1 &
 python3 ./python/car_bt.py >/dev/null 2>&1 &
