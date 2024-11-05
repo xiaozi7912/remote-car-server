c95274d425ea9de7ed7c98c5df882973bd5ba76a
Larry Liu <xiaozi7912@gmail.com>
2024-11-05 22:29:45 +0800

    Modified version 1.0.0

--- python/car_bt.py
+++ python/car_bt.py
@@ -2,53 +2,228 @@ import bluetooth
 from gpiozero import OutputDevice
 import time
 import threading
+import json
 
-accelertor = OutputDevice(17)
-light = OutputDevice(27)
+motor_a = OutputDevice(pin=17, initial_value=True)
+motor_b = OutputDevice(pin=18, initial_value=True)
+accelertor = OutputDevice(pin=27, initial_value=False)
+light_1 = OutputDevice(pin=22, initial_value=False)
+light_2 = OutputDevice(pin=23, initial_value=False)
+light_3 = OutputDevice(pin=9, initial_value=False)
+light_4 = OutputDevice(pin=25, initial_value=False)
+horn = OutputDevice(pin=11, initial_value=False)
+music = OutputDevice(pin=8, initial_value=False)
+
+direction = 1
+light_status = 0
 is_light_blink = False
+light_thread = None
+
 
 def handle_command(command):
-    global is_light_blink
-    if command == 'start':
-        print('Motor started')
+    global direction, light_status, is_light_blink, light_thread
+
+    if command == "forward":
+        direction = 1
+        motor_a.on()
+        motor_b.on()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "Motor forward",
+        }
+
+    elif command == "backward":
+        direction = 2
+        motor_a.off()
+        motor_b.off()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "Motor backward",
+        }
+
+    elif command == "start":
         accelertor.on()
-        return 'Motor started'
-    
-    elif command == 'stop':
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "Motor started",
+        }
+
+    elif command == "stop":
         accelertor.off()
-        print('Motor stopped')
-        return 'Motor stopped'
-    
-    elif command == 'light_on':
-        light.on()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "Motor stopped",
+        }
+
+    elif command == "front_light_on":
+        if light_status > 3:
+            turn_off_all_light()
+
+        light_status += 1
         is_light_blink = False
-        print('Light turned on')
-        return 'Light turned on'
-    
-    elif command == 'light_off':
-        light.off()
+        light_1.on()
+        light_2.on()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "front_light_on",
+        }
+
+    elif command == "back_light_on":
+        if light_status > 3:
+            turn_off_all_light()
+
+        light_status += 2
         is_light_blink = False
-        print('Light turned off')
-        return 'Light turned off'
-    
-    elif command == 'light_blink':
-        if not is_light_blink:
-            is_light_blink = True
-            light_thread = threading.Thread(target=light_blink)
-            light_thread.start()
-        print('Light blink')
-        return 'Light blink'
-    
+        light_3.on()
+        light_4.on()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "back_light_on",
+        }
+
+    elif command == "light_left_blink":
+        turn_off_all_light()
+        is_light_blink = True
+        light_status = 4
+        light_thread = threading.Thread(target=light_left_blink)
+        light_thread.start()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "light_left_blink",
+        }
+
+    elif command == "light_right_blink":
+        turn_off_all_light()
+        is_light_blink = True
+        light_status = 5
+        light_thread = threading.Thread(target=light_right_blink)
+        light_thread.start()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "light_right_blink",
+        }
+
+    elif command == "light_blink":
+        turn_off_all_light()
+        is_light_blink = True
+        light_status = 6
+        light_thread = threading.Thread(target=light_blink)
+        light_thread.start()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "light_blink",
+        }
+
+    elif command == "light_off":
+        turn_off_all_light()
+        light_status = 0
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "light_off",
+        }
+
+    elif command == "speaker_horn":
+        horn.on()
+        time.sleep(0.1)
+        horn.off()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "Speaker horn",
+        }
+
+    elif command == "speaker_music":
+        music.on()
+        time.sleep(0.1)
+        music.off()
+        response = {
+            "status": True,
+            "direction": direction,
+            "light": light_status,
+            "message": "Speaker music",
+        }
+
     else:
-        return 'Invalid command'
-    
+        response = {"status": False, "message": "Invalid command"}
+
+    print(response)
+    return json.dumps(response)
+
+
+def turn_off_all_light():
+    global light_status, is_light_blink, light_thread
+    is_light_blink = False
+    light_status = 0
+
+    if light_thread is not None:
+        light_thread.join()
+
+    light_1.off()
+    light_2.off()
+    light_3.off()
+    light_4.off()
+
+
+def light_left_blink():
+    global is_light_blink
+    while is_light_blink:
+        print("light_left_blink")
+        light_1.on()
+        light_3.on()
+        time.sleep(0.5)
+        light_1.off()
+        light_3.off()
+        time.sleep(0.5)
+
+
+def light_right_blink():
+    global is_light_blink
+    while is_light_blink:
+        print("light_right_blink")
+        light_2.on()
+        light_4.on()
+        time.sleep(0.5)
+        light_2.off()
+        light_4.off()
+        time.sleep(0.5)
+
+
 def light_blink():
     global is_light_blink
     while is_light_blink:
-        light.on()
+        print("light_blink")
+        light_1.on()
+        light_2.on()
+        light_3.on()
+        light_4.on()
         time.sleep(0.5)
-        light.off()
+        light_1.off()
+        light_2.off()
+        light_3.off()
+        light_4.off()
         time.sleep(0.5)
+
 
 def start_bluetooth_server():
     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -56,38 +231,51 @@ def start_bluetooth_server():
     server_sock.bind(("", port))
     server_sock.listen(1)
 
-    bluetooth.advertise_service(server_sock, "MotorControlService",
-                                service_id="00001101-0000-1000-8000-00805F9B34FB",
-                                service_classes=[bluetooth.SERIAL_PORT_CLASS],
-                                profiles=[bluetooth.SERIAL_PORT_PROFILE])
+    bluetooth.advertise_service(
+        server_sock,
+        "MotorControlService",
+        service_id="00001101-0000-1000-8000-00805F9B34FB",
+        service_classes=[bluetooth.SERIAL_PORT_CLASS],
+        profiles=[bluetooth.SERIAL_PORT_PROFILE],
+    )
 
     print("Waiting for connection...")
 
     while True:
         client_sock, address = server_sock.accept()
+        response = json.dumps(
+            {
+                "status": True,
+                "direction": direction,
+                "light": light_status,
+                "message": "Connected",
+            }
+        )
+        client_sock.send(response.encode("utf-8"))
         print(f"Connected to {address}")
 
         try:
             while True:
-                data = client_sock.recv(1024).decode('utf-8')
+                data = client_sock.recv(1024).decode("utf-8")
                 if not data:
                     print("Client disconnected.")
-                    break 
+                    break
 
                 response = handle_command(data.strip())
-                client_sock.send(response.encode('utf-8'))
+                client_sock.send(response.encode("utf-8"))
 
         except bluetooth.btcommon.BluetoothError as e:
             print("Bluetooth error:", e)
 
         except OSError as e:
             print("OS error:", e)
-            break 
+            break
 
         print("Closing connection")
-        client_sock.close() 
+        client_sock.close()
 
-    server_sock.close() 
+    server_sock.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start_bluetooth_server()
--- python/car_http.py
+++ python/car_http.py
@@ -1,39 +1,230 @@
 from flask import Flask, request, jsonify
 from gpiozero import OutputDevice
+import time
+import threading
 
 app = Flask(__name__)
 
-accelertor = OutputDevice(17)
-light = OutputDevice(27)
+motor_a = OutputDevice(pin=17, initial_value=True)
+motor_b = OutputDevice(pin=18, initial_value=True)
+accelertor = OutputDevice(pin=27, initial_value=False)
+light_1 = OutputDevice(pin=22, initial_value=False)
+light_2 = OutputDevice(pin=23, initial_value=False)
+light_3 = OutputDevice(pin=25, initial_value=False)
+light_4 = OutputDevice(pin=9, initial_value=False)
+horn = OutputDevice(pin=11, initial_value=False)
+music = OutputDevice(pin=8, initial_value=False)
 
-@app.route('/start', methods=['POST'])
-def start_motor():
-    accelertor.on() 
-    return jsonify({'status': 'success', 'message': '馬達啟動'}), 200
+direction = 1
+light_status = 0
+is_light_blink = False
+light_thread = None
 
-@app.route('/stop', methods=['POST'])
-def stop_motor():
-    accelertor.off() 
-    return jsonify({'status': 'success', 'message': '馬達停止'}), 200
+@app.route('/motor', methods=['POST'])
+def motor():
+    global direction, light_status
+    data = request.get_json() 
+
+    if 'cmd' in data:
+        cmd = data['cmd']
+        value = data['value']
+        match cmd:
+            case 'accelertor':
+                match value:
+                    case 1:
+                        accelertor.on()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '馬達啟動'}), 200
+                    case _:
+                        accelertor.off()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '馬達停止'}), 200
+            case 'direction':
+                match value:
+                    case 1:
+                        direction=1
+                        motor_a.on()
+                        motor_b.on()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '前進'}), 200
+                    case 2:
+                        direction=2
+                        motor_a.off()
+                        motor_b.off()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '後退'}), 200
+    else:
+        response= jsonify({'code': 400, 'message': '缺少參數'}), 400
+    return response
+    
 
 @app.route('/light', methods=['POST'])
-def switch_light():
-    data = request.get_json() 
-    if 'on' in data:
-        on = data['on'] 
-        if on:
-            light.on()
-            return jsonify({'status': 'success', 'message': '燈已開啟'}), 200
-        else:
-            light.off()
-            return jsonify({'status': 'success', 'message': '燈已關閉'}), 200
+def light():
+    global direction, light_status, is_light_blink, light_thread
+    data = request.get_json()
+
+    if 'cmd' in data:
+        cmd = data['cmd'] 
+        value = data['value']
+        match cmd:
+            case 'all':
+                match value:
+                    case 0:
+                        turn_off_all_light()
+                        light_status =0
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '燈全關'}), 200
+                    case 1:
+                        turn_on_all_light()
+                        light_status =3
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '燈全開'}), 200
+            case 'front_light':
+                turn_off_all_light()
+                match value:
+                    case 1:
+                        light_status =1
+                        light_1.on()
+                        light_2.on()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '開前燈'}), 200
+                    case _:
+                        response= jsonify({'code': 400,'direction':direction,'light_status':light_status, 'message': '數值錯誤'}), 400
+            case 'back_light':
+                turn_off_all_light()
+                match value:
+                    case 1:
+                        light_status =2
+                        light_3.on()
+                        light_4.on()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '開後燈'}), 200
+                    case _:
+                        response= jsonify({'code': 400,'direction':direction,'light_status':light_status, 'message': '數值錯誤'}), 400
+            case 'turn_left':
+                turn_off_all_light()
+                match value:
+                    case 1:
+                        light_status = 4
+                        is_light_blink = True
+                        light_thread = threading.Thread(target=turn_left_light)
+                        light_thread.start()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '左轉燈'}), 200
+                    case _:
+                        response= jsonify({'code': 400,'direction':direction,'light_status':light_status, 'message': '數值錯誤'}), 400
+            case 'turn_right':
+                turn_off_all_light()
+                match value:
+                    case 1:
+                        light_status = 5
+                        is_light_blink = True
+                        light_thread = threading.Thread(target=turn_right_light)
+                        light_thread.start()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '右轉燈'}), 200
+                    case _:
+                        response= jsonify({'code': 400,'direction':direction,'light_status':light_status, 'message': '數值錯誤'}), 400
+            case 'parking':
+                turn_off_all_light()
+                match value:
+                    case 1:
+                        light_status = 6
+                        is_light_blink = True
+                        light_thread = threading.Thread(target=parking_light)
+                        light_thread.start()
+                        response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '臨停燈'}), 200
+                    case _:
+                        response= jsonify({'code': 400,'direction':direction,'light_status':light_status, 'message': '數值錯誤'}), 400
     else:
-        return jsonify({'status': 'error', 'message': '缺少參數'}), 400
+        response= jsonify({'code': 400, 'message': '缺少參數'}), 400
+    return response
+
+@app.route('/sound', methods=['POST'])
+def sound():
+    global direction, light_status, is_light_blink, light_thread
+    data = request.get_json()
+
+    if 'cmd' in data:
+        cmd = data['cmd'] 
+        value = data['value']
+
+        match cmd:
+            case 'music':
+                music.on()
+                time.sleep(0.1)
+                music.off()
+                response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '播放音樂'}), 200
+            case 'horn':
+                horn.on()
+                time.sleep(0.1)
+                horn.off()
+                response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': '喇叭'}), 200
+    else:
+        response= jsonify({'code': 400, 'message': '缺少參數'}), 400
+    return response
 
 @app.route('/status', methods=['GET'])
 def get_status():
-    status = 'on' if accelertor.is_active else 'off'
-    return jsonify({'status': status}), 200
+    response= jsonify({'code': 0,'direction':direction,'light_status':light_status, 'message': ''}), 200
+    return response
+
+@app.after_request
+def log_request(response):
+    try:
+        data = request.get_json()
+        print("Request:" ,data)
+    except:
+        pass
+    return response
+
+def turn_on_all_light():
+    global light_status, is_light_blink, light_thread
+    is_light_blink = False
+
+    if light_thread is not None:
+        light_thread.join()
+
+    light_1.on()
+    light_2.on()
+    light_3.on()
+    light_4.on()
+
+def turn_off_all_light():
+    global light_status, is_light_blink, light_thread
+    is_light_blink = False
+
+    if light_thread is not None:
+        light_thread.join()
+
+    light_1.off()
+    light_2.off()
+    light_3.off()
+    light_4.off()
+
+def turn_left_light():
+    global is_light_blink
+    while is_light_blink:
+        light_1.on()
+        light_3.on()
+        time.sleep(0.5)
+        light_1.off()
+        light_3.off()
+        time.sleep(0.5)
+
+def turn_right_light():
+    global is_light_blink
+    while is_light_blink:
+        light_2.on()
+        light_4.on()
+        time.sleep(0.5)
+        light_2.off()
+        light_4.off()
+        time.sleep(0.5)
+
+def parking_light():
+    global is_light_blink
+    while is_light_blink:
+        light_1.on()
+        light_2.on()
+        light_3.on()
+        light_4.on()
+        time.sleep(0.5)
+        light_1.off()
+        light_2.off()
+        light_3.off()
+        light_4.off()
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
